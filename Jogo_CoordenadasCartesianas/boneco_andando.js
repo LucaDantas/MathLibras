17d9ab029b6b59b038409bf267cd753d5917e9dc
LucaDantas <luca100sdantas2@gmail.com>
2019-12-30 11:21:25 -0300

    Fixed the rotation problem and added a nose to it

--- Jogo_CoordenadasCartesianas/boneco_andando.js
+++ Jogo_CoordenadasCartesianas/boneco_andando.js
@@ -9,35 +9,23 @@ function circle(px, py, radius, stroke, colorfill, colorstroke){
 	ctx.stroke();
 }
 
-function rectangleRound(px, py, w, h, r, stroke, strokefill, colorfill, rotation){
-    ctx.translate(px,py);
-	ctx.rotate(rotation*Math.PI/180); // Entre com o valor em graus		
-    ctx.translate(-px,-py);
-    
-	var pontox = [px, px + r, px + w - r, px + w];
-	var pontoy = [py, py - r, py - h + r, py - h]; 
-	ctx.beginPath();
-	ctx.lineCap="round";
-	ctx.setLineDash([1,0]);
-	ctx.moveTo(pontox[0], pontoy[1]);
-	ctx.lineTo(pontox[0], pontoy[2]);			
-	ctx.quadraticCurveTo(pontox[0], pontoy[3], pontox[1], pontoy[3]);			
-	ctx.lineTo(pontox[2], pontoy[3]);			
-	ctx.quadraticCurveTo(pontox[3], pontoy[3], pontox[3], pontoy[2]);
-	ctx.lineTo(pontox[3], pontoy[1]);			
-	ctx.quadraticCurveTo(pontox[3], pontoy[0], pontox[2], pontoy[0]);			
-	ctx.lineTo(pontox[1], pontoy[0]);			
-	ctx.quadraticCurveTo(pontox[0], pontoy[0], pontox[0], pontoy[1]);	 
-	ctx.lineWidth = stroke;
-	ctx.strokeStyle = strokefill;	
-	ctx.fillStyle = colorfill;
-	ctx.fill();				
-    ctx.stroke();
-    
-    ctx.restore();
-}
 
-function corpoParado(px, py, lenght, colorhead, colorarms, colorfeet, rotation){
-    rectangleRound(px - lenght/2, py + lenght/11 , lenght, lenght/5 , lenght/8, 0, nocolor, colorarms, rotation);
-    circle(px, py, lenght/5, 0, colorhead, nocolor);
+function corpoParado(px, py, lenght, colorhead, colorarms, colorfeet, colorskin, deg){
+	ctx.save();
+	var rad = deg*Math.PI/180;
+	ctx.translate(px, py);
+	ctx.rotate(rad);
+	//
+
+	//ARMS
+	rectangleRound(-lenght/2, lenght/8 , lenght, lenght/4 , lenght/6, 0, nocolor, colorarms);
+	//HEAD
+	triangle(-lenght/14, -lenght/6, lenght/14, -lenght/6, 0, -lenght/7 - lenght/7, 0, colorskin, nocolor);
+	circle(0, 0, lenght/5, 0, colorhead, nocolor);
+	//FEET
+
+
+	//
+	ctx.translate(-px, -py);
+	ctx.restore();
 }
