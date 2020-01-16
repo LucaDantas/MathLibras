14f47f9f44ea66aec093087107bf004c86d0dd7a
LucaDantas <luca100sdantas2@gmail.com>
2020-01-16 15:23:10 -0300

    Started working on the one that walks and added the test on the other file .html

--- Jogo_CoordenadasCartesianas/boneco_andando.js
+++ Jogo_CoordenadasCartesianas/boneco_andando.js
@@ -10,7 +10,7 @@ function circle(px, py, radius, stroke, colorfill, colorstroke){
 }
 
 
-function corpoParado(px, py, lenght, colorhead, colorarms, colorfeet, colorskin, deg){
+function corpoParado(px, py, lenght, colorhead, colorarms, colorskin, deg){
 	ctx.save();
 	var rad = deg*Math.PI/180;
 	ctx.translate(px, py);
@@ -22,8 +22,24 @@ function corpoParado(px, py, lenght, colorhead, colorarms, colorfeet, colorskin,
 	//HEAD
 	triangle(-lenght/14, -lenght/6, lenght/14, -lenght/6, 0, -lenght/7 - lenght/7, 0, colorskin, nocolor);
 	circle(0, 0, lenght/5, 0, colorhead, nocolor);
-	//FEET
 
+	ctx.translate(-px, -py);
+	ctx.restore();
+}
+
+//THE ONLY DIFFERENCE BETWEEN THIS ONE AND THE ONE STANDING STILL IS THAT THIS ONE WILL HAVE FEET THAT MOVE IN DIFFERENT POSITIONS BASED TO PASS THE IDEA THAT IT'S MOVING
+function andando(px, py, lenght, colorhead, colorarms, colorskin, colorfeet, position, deg){
+	ctx.save();
+	var rad = deg*Math.PI/180;
+	ctx.translate(px, py);
+	ctx.rotate(rad);
+	//
+
+	//FEET
+	rectangleRound(lenght/6, position + lenght/8.5, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
+	rectangleRound(-lenght/3.2, -position + lenght/8.5, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
+
+	corpoParado(0, 0, lenght, colorhead, colorarms, colorskin, 0);
 
 	//
 	ctx.translate(-px, -py);
