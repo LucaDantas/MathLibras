f7c3d12666b967ea3513d84f98483ebfa225e1b4
LucaDantas <luca100sdantas2@gmail.com>
2020-01-20 12:12:42 -0300

    Changed the standing still character adding feet to it

--- Jogo_CoordenadasCartesianas/boneco_andando.js
+++ Jogo_CoordenadasCartesianas/boneco_andando.js
@@ -10,7 +10,27 @@ function circle(px, py, radius, stroke, colorfill, colorstroke){
 }
 
 
-function corpoParado(px, py, lenght, colorhead, colorarms, colorskin, deg){
+function corpoParado(px, py, lenght, colorhead, colorarms, colorskin, colorfeet, deg){
+	ctx.save();
+	var rad = deg*Math.PI/180;
+	ctx.translate(px, py);
+	ctx.rotate(rad);
+	//
+
+	//FEET
+	rectangleRound(lenght/5.8, 0 , lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
+	rectangleRound(-lenght/3.2, 0, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
+	//ARMS
+	rectangleRound(-lenght/2, lenght/8 , lenght, lenght/4 , lenght/6, 0, nocolor, colorarms);
+	//HEAD
+	triangle(-lenght/14, -lenght/6, lenght/14, -lenght/6, 0, -lenght/7 - lenght/7, 0, colorskin, nocolor);
+	circle(0, 0, lenght/5, 0, colorhead, nocolor);
+
+
+	ctx.translate(-px, -py);
+	ctx.restore();
+}
+function corpoParadoNoFeet(px, py, lenght, colorhead, colorarms, colorskin, deg){
 	ctx.save();
 	var rad = deg*Math.PI/180;
 	ctx.translate(px, py);
@@ -22,6 +42,7 @@ function corpoParado(px, py, lenght, colorhead, colorarms, colorskin, deg){
 	//HEAD
 	triangle(-lenght/14, -lenght/6, lenght/14, -lenght/6, 0, -lenght/7 - lenght/7, 0, colorskin, nocolor);
 	circle(0, 0, lenght/5, 0, colorhead, nocolor);
+
 
 	ctx.translate(-px, -py);
 	ctx.restore();
@@ -43,22 +64,22 @@ function andando(px, py, lenght, colorhead, colorarms, colorskin, colorfeet, pos
 			chg = true;
 		}
 		if(!side){
-			rectangleRound(lenght/6, position%( Math.floor(lenght/7) +1) + lenght/8.5 - lenght/7, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
+			rectangleRound(lenght/5.8, position%( Math.floor(lenght/7) +1) + lenght/8.5 - lenght/7, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
 			rectangleRound(-lenght/3.2, -position%( Math.floor(lenght/7) +1) + lenght/8.5 + lenght/7, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
 		}
 		else{
-			rectangleRound(lenght/6, -position%( Math.floor(lenght/7) +1) + lenght/8.5 + lenght/7, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
+			rectangleRound(lenght/5.8, -position%( Math.floor(lenght/7) +1) + lenght/8.5 + lenght/7, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
 			rectangleRound(-lenght/3.2, position%( Math.floor(lenght/7) +1) + lenght/8.5 - lenght/7, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
 		}
 	}
 	else{
 		if(side) position = -position;
 		chg = false;
-		rectangleRound(lenght/6, position%( Math.floor(lenght/7) +1) + lenght/8.5, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
+		rectangleRound(lenght/5.8, position%( Math.floor(lenght/7) +1) + lenght/8.5, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
 		rectangleRound(-lenght/3.2, -position%( Math.floor(lenght/7) +1) + lenght/8.5, lenght/7, lenght/4.5, lenght/10, 0, nocolor, colorfeet);
 	}
 	
-	corpoParado(0, 0, lenght, colorhead, colorarms, colorskin, 0);
+	corpoParadoNoFeet(0, 0, lenght, colorhead, colorarms, colorskin, 0);
 
 	//
 	ctx.translate(-px, -py);
